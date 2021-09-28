5027b71d6a3d36adb7bd1c3f3fb3165adb78cf6a
SeoTaeyoung <sty4725@naver.com>
2021-09-28 11:01:54 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>/', views.single_post_page),
     path('', views.index), # 아무것도 없을 시 blog 리스트를 보여주는 기능으로 이동함
 ]
