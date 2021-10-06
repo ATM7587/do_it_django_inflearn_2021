1acdb36df2aaeb4deba54d430be20da2339eb3ab
seotaeyoung <sty4725@naver.com>
2021-10-06 16:06:23 +0900

    네비게이션 바와 푸터 모듈화(include)

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,23 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_post_list(self):
         self.assertEqual(2, 2)
 
@@ -17,13 +34,10 @@ class TestView(TestCase):
         # 1.2 정상적으로 페이지가 로드된다.
         self.assertEqual(response.status_code, 200)
         # 1.3 페이지의 타이틀에 Blog라는 문구가 있다.
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, 'html.parser') #html.parser를 사용해서 soup에 넣을 것
         self.assertIn('Blog',soup.title.text)
-        # 1.4 nav 바가 있다
-        navbar = soup.nav
-        # 1.5 Blog, About me 라는 문구가 Navbar에 있다.
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+
+        self.navbar_test(soup)
 
         # 2.1 게시물이 하나도 없을 때
         self.assertEqual(Post.objects.count(), 0) # 몇 개의 게시물이 있는지 센다.
@@ -69,9 +83,8 @@ class TestView(TestCase):
 
         soup = BeautifulSoup(response.content, 'html.parser')
         # 2.2 포스트 목록 페이지와 똑같은 네비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+
+        self.navbar_test(soup)
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
         self.assertIn(post_001.title, soup.title.text)
