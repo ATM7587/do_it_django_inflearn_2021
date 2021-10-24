412fe6acfb50e7fa2837f118d9bd7a02a4367e38
seotaeyoung <sty4725@naver.com>
2021-10-24 17:52:25 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 
 class PostList(ListView):
@@ -24,10 +25,17 @@ class PostDetail(DetailView):
         return context
 
 
-class PostCreate(CreateView): # 글 작성 페이지에 있으면 하는 것들을 Post 모델에서 선택할 것
+class PostCreate(LoginRequiredMixin, CreateView): # 글 작성 페이지에 있으면 하는 것들을 Post 모델에서 선택할 것
     model = Post # template 이름을 지정하지 않을 시 post_form.html 을 기본값으로 가지게 됨
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def form_valid(self, form):
+        current_user = self.request.user # request : 요청을 하는 클라이언트에 대한 정보를 담음
+        if current_user.is_authenticated: # 로그인 한 상태면 True를 반환, 로그인하지 않은 상태면 False를 반환
+            form.instance.author = current_user # form 클래스로 만들어진 인스턴스의 author 필드에 current_user를 채워넣는다.
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/') # 로그인하지 않았을 경우에는 /blog/로 보낸다.
 
 def category_page(request, slug):
     if slug == 'no_category':
